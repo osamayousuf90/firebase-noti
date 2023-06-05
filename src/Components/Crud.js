24b9960262b59a94a42e43e7b378293d57e63913
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2023-06-05 12:00:45 +0500

    fixed

--- src/Components/Crud.js
+++ src/Components/Crud.js
@@ -23,26 +23,51 @@ const Crud = () => {
 
   // handle func
   const handleMe = (item, type, index) => {
-    const checkIfExist = tempValue?.find((res) => res?.id === item?.id);
-    list[index].type = type;
-    if (checkIfExist) {
-      if (item?.type === type) {
-        console.log("item type not same");
-        setTempValue(tempValue?.filter((res) => res?.id !== item?.id));
-        setUpdate(!update);
-        list[index].type = 0;
-      } 
-      // setUpdate(!update);
-    } else {
-      console.log("item not exist");
-      const itemNotExistBody = {
-        ...item,
-        type,
-      };
-      tempValue.push(itemNotExistBody);
+    const findIfIdExist = tempValue?.find((res) => res?.id === item?.id);
+    if (findIfIdExist) {
+      setTempValue(tempValue?.filter((res) => res?.id !== item?.id));
+      list[index].type = type;
       setUpdate(!update);
+    } else {
+      const findIfTypeExist = tempValue?.find(
+        (res) => res?.type === item?.type
+      );
+      if (findIfTypeExist) {
+        console.log("findIfTypeExist", findIfTypeExist);
+      } else {
+        const body = {
+          ...item,
+          type,
+        };
+        list[index].type = type;
+        tempValue.push(body);
+        setUpdate(!update);
+      }
     }
   };
+
+  console.log("tempValue", tempValue);
+
+  // const checkIfExistById = tempValue?.find((res) => res?.id === item?.id);
+  // const checkIfTypeSame = tempValue?.some((res) => res?.type === type);
+  // console.log("checkIfTypeSame" ,checkIfTypeSame)
+  // list[index].type = type;
+  // setUpdate(!update);
+  // if (checkIfExistById) {
+  //   setTempValue(tempValue?.filter((res) => res?.id !== item?.id));
+  //   setUpdate(!update);
+  //   list[index].type = 0;
+  // } else if (checkIfTypeSame) {
+  //   console.log("checkIfTypeSame", checkIfTypeSame);
+  // } else {
+  //   console.log("item not exist");
+  //   const itemNotExistBody = {
+  //     ...item,
+  //     type,
+  //   };
+  //   tempValue.push(itemNotExistBody);
+  //   setUpdate(!update);
+  // }
 
   return (
     <div>
